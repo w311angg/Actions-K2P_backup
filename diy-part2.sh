28e7f962536bf378f28e3ae4ef2af8859d445a3e
w311ang <30381083+w311ang@users.noreply.github.com>
2021-09-08 22:52:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+sed -i "s/enable '0'/enable '1'/g" package/dns2tcp/files/dns2tcp.config
+
+sed -i "s/enable '0'/enable '1'/g" package/chinadns-ng/files/chinadns-ng.config
+sed -i "s/bind_addr '0.0.0.0'/bind_addr '127.0.0.1'/g" package/chinadns-ng/files/chinadns-ng.config
+sed -i "s/bind_port '5353'/bind_port '5335'/g" package/chinadns-ng/files/chinadns-ng.config
