2d8be031b561e160d65ecfdda007801eaf7e971a
w311ang <30381083+w311ang@users.noreply.github.com>
2021-09-10 21:11:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+sed -i '/-j REDIRECT --to-ports 53/d' package/lean/default-settings/files/zzz-default-settings
