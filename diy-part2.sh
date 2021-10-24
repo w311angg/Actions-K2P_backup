22cc6e11079530dd409bc76e2cedc79523bb57a8
w311ang <30381083+w311ang@users.noreply.github.com>
2021-10-24 13:37:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# Modify default settings
 echo >> default-settings
 sed -n -i -e '/rm -rf \/tmp\/luci-modulecache\//r default-settings' -e 1x -e '2,${x;p}' -e '${x;p}' package/lean/default-settings/files/zzz-default-settings
+
+# 删除对于防火墙的修改
+sed -i "/REDIRECT --to-ports 53' >>/d" package/lean/default-settings/files/zzz-default-settings
